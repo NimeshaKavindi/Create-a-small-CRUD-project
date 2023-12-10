768aafad3edcf1af482d7c24874e0080ac9a6a8b
Nimesha Kavindi <abeykoo-se19021@stu.kln.ac.lk>
2023-12-10 13:15:11 +0530

    create home page

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyAH5XDD2QINCkFLBEkeKVyjsRONKUWTb_Y",
@@ -13,4 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const database = getAuth(app)
+export const database = getAuth(app);
+export const db = getFirestore(app);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import {database} from "../firebase";
 import {signOut} from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { Form, Alert, InputGroup, Button, ButtonGroup } from "react-bootstrap";
+import BookDataService from "../services/book.services";
+
 
 function Home(){
 
     const history = useNavigate()
-
     const handleClick = ()=>{
         signOut(database).then(val =>{
             console.log(val,"val")
            history('/')
         })
     }
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [status, setStatus] = useState("Available");
+    const [flag, setFlag] = useState(true);
+    const [message, setMessage] = useState({error: false, msg:""});
 
     
     return(
@@ -24,7 +31,62 @@ function Home(){
             </button>
           </div>
         </div>
-        {/* Rest of your component content */}
+        <div className="p-4 box">
+         {/* This is a comment about using the Alert component from Bootstrap */}
+         {message?.msg && (
+          <Alert
+            variant={message?.error ? "danger" : "success"}
+            dismissible
+            onClose={() => setMessage("")}
+          >
+            {message?.msg}
+          </Alert>
+        )}
+
+
+            <Form onSubmit={}>
+              <Form.Group className="mb-3" controlId="formBookTitle">
+                <InputGroup>
+                  <InputGroup.Text id="formBookTitle">B</InputGroup.Text>
+                  <Form.Control type="text" placeholder="Book Title" value = {title} onChange={(e) => setTitle(e.target.value)}/>
+                </InputGroup>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBookAuthor">
+                <InputGroup>
+                  <InputGroup.Text id="formBookAuthor">A</InputGroup.Text>
+                  <Form.Control type="text" placeholder="Book Author" value ={author} onChange={(e) => setAuthor(e.target.value)} />
+                </InputGroup>
+              </Form.Group>
+              <ButtonGroup aria-label="Basic example" className="mb-3">
+                <Button
+                  disabled={flag}
+                  variant="success"
+                  onClick={(e) => {
+                    setStatus("Available");
+                    setFlag(true);
+                  }}
+                >
+                  Available
+                </Button>
+                <Button
+                  variant="danger"
+                  disabled={!flag}
+                  onClick={(e) => {
+                    setStatus("Not Available");
+                    setFlag(false);
+                  }}
+                >
+                  Not Available
+                </Button>
+              </ButtonGroup>
+              <div className="d-grid gap-2">
+                <Button variant="primary" type="Submit">
+                  Add/ Update
+                </Button>
+              </div>
+            </Form>
+      </div>
       </div>
     )
 }
